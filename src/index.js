f1b61f4ff42d8caf76bbd441edc85b5f4f00709e
Sebastian Opperman <sebastian@bnry.digital>
2018-04-05 15:50:50 +0200

    ready

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const form = document.getElementById('form');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  setFullname(getName(), getSurname());
+  // fullname function here
 });
