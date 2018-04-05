27fe499ef919572876fe9f7a5f5e63dba6d790f8
Sebastian Opperman <sebastian@bnry.digital>
2018-04-05 15:49:48 +0200

    ready

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import setFullname from './components/fullname';
-import { getName, getSurname } from './components/user';
 const form = document.getElementById('form');
 
 form.addEventListener('submit', e => {
